2a98f79b0fd30ca49e7559f50bab864681047090
emacadocious <ericmcallister1@gmail.com>
2020-07-20 15:09:13 -0600

    updating put statement

--- update.js
+++ update.js
@@ -3,21 +3,24 @@ import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
+  const userId = event.requestContext.identity.cognitoIdentityId;
   const params = {
     TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be updated
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'itemId': path parameter
     Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
+      userId,
       itemId: event.pathParameters.id
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET content = :content, attachment = :attachment",
+    UpdateExpression: "SET firstName = :firstName, lastName = :lastName",
     ExpressionAttributeValues: {
-      ":attachment": data.attachment || null,
-      ":content": data.content || null
+      ":firstName": data.firstName || null,
+      ":lastName": data.lastName || null,
+      ":purchased": data.purchased || null,
+      ":userId": userId
     },
     // 'ReturnValues' specifies if and how to return the item's attributes,
     // where ALL_NEW returns all attributes of the item after the update; you
